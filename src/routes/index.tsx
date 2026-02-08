effef907de13050ea06eb55ec8159fbccda95d54
aiarnob23 <aiarnob23@gmail.com>
2026-02-08 16:24:23 +0600

    inspectra frontend basic set up done

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router";
+
+
+const router = createBrowserRouter([
+    {
+        path:"/",
+        element:<div>Inspectra Frontend</div>
+    }
+])
+
+export default router;
